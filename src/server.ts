a1fe1668bd104677792efa714751faafb6f300d5
IvanGarciaSantos <ivangarciasantos2006@gmail.com>
2026-03-06 13:34:57 +0100

    feat: Initialize Express server in `src/server.ts` and update project entry points and dependencies.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
+import 'reflect-metadata';
 import express from 'express';
-
-const PORT = process.env.PORT;
+// import { connectDB } from './infrastructure/config/postgres.config.js'; // TODO: Create database config
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Middlewares
+app.use(express.json());
+
+// Routes
+import { userRouter } from './gameLogic/infrastructure/routes/user.routes.js';
+
+app.use('/api/user', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Server is running correctly! 🚀');
+});
+
+app.listen(PORT, async () => {
+    // await connectDB(); // TODO: Uncomment when database config is ready
+    console.log(`Server running on: http://localhost:${PORT}`);
 });
