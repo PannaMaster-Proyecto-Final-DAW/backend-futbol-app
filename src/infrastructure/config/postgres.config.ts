6ee26aa1d4d2b9b71826427e79c4c226a4b72b22
IvanGarciaSantos <ivangarciasantos2006@gmail.com>
2026-03-17 14:09:00 +0100

    feat: Add initial PostgreSQL database configuration and connection setup using Sequelize.

--- src/infrastructure/config/postgres.config.ts
+++ src/infrastructure/config/postgres.config.ts
@@ -24,7 +24,7 @@ const sequelize = new Sequelize({
 export const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log('Database connected successfully 🚀');
+        console.log('Database connected successfully');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
