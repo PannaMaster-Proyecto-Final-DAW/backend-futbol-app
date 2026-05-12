008090345579b6015f469cfc6839b24336f97990
IvanGarciaSantos <ivangarciasantos2006@gmail.com>
2026-05-12 09:31:41 +0200

    feat: implement Sequelize database configuration and CacheService wrapper

--- src/infrastructure/config/sequelize.config.cjs
+++ src/infrastructure/config/sequelize.config.cjs
@@ -8,6 +8,11 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: process.env.DB_USER,
